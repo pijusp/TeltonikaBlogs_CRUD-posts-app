848b2009006e8a2551eccab23780d53496507439
Pijus <p.pozera@gmail.com>
2023-07-24 13:07:43 +0300

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="app-wrapper">
         <div class="app">
+            <Navigation />
             <router-view />
         </div>
     </div>
 </template>
 
 <script>
+import Navigation from "./components/Navigation.vue";
 export default {
     name: "app",
-    components: {},
+    components: { Navigation },
     data() {
         return {};
     },
@@ -21,13 +23,13 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@30,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap");
-
+body {
+    font-family: "Poppins", sans-serif;
+}
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: "Quicksand", sans-serif;
 }
 
 .app {
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    chainWebpack: (config) => {
+        config.module.rules.delete("svg");
+        config.module
+            .rule("svg")
+            .test(/\.(svg)(\?.*)?$/)
+            .use("babel-loader")
+            .loader("babel-loader")
+            .end()
+            .use("vue-svg-loader")
+            .loader("vue-svg-loader");
+    },
+};
